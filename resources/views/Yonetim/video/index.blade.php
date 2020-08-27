59a261197900a57946403d607775bcd19b6769f9
ismail Hakki yildiz <dupi@live.com>
2020-08-27 23:28:35 +0300

    fix video

--- resources/views/Yonetim/video/index.blade.php
+++ resources/views/Yonetim/video/index.blade.php
@@ -34,7 +34,9 @@
                             @foreach($videolar as $v)
                                 <tr>
                                     <th scope="row">{{$loop->index+1}}.</th>
-                                    <td class="p-2 m-0" style="width:17%"><iframe width="200" height="120" src="{{$v->youtubelink}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+                                    <td class="p-2 m-0" style="width:17%">
+                                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/{{str_replace("www.youtube.com/watch?v=","",$v->youtubelink)}}?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    </td>
                                     <td>{{dugunsalonu($v->salon_id)}}</td>
                                     <td class="w-25">
                                         {{$v->aciklama}}
